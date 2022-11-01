8a399f6f540d085f43def3f36d8f02779c0bf0de
ahmad199617 <95858731+ahmad199617@users.noreply.github.com>
2022-11-01 14:22:00 +0300

    test

--- References.js
+++ References.js
@@ -24,58 +24,102 @@ document.addEventListener("DOMContentLoaded", function(){
 
    
     {
-    $('[lang="tr"]').show();
-    $('[lang="en"]').hide();
-    $('[lang="ar"]').hide();}    
-   
-    var lang=localStorage.getItem('lang');
-    localStorage.getItem('lang');   
-    if(lang==("en")){
-    $('[lang="en"]').show();
-    $('[lang="tr"]').hide();
-    $('[lang="ar"]').hide();}
-    
-    
-    if(lang==("tr")){
-    $('[lang="tr"]').show();
-    $('[lang="en"]').hide();
-    $('[lang="ar"]').hide();}
-    
-    if(lang==("ar")){
-    $('[lang="ar"]').show();
-    $('[lang="en"]').hide();
-    $('[lang="tr"]').hide();}});
+        document.documentElement.setAttribute("lang", 'tr');
+        $('[lang="tr"]').show();
+        $('[lang="en"]').hide();
+        $('[lang="ar"]').hide();
+       
+    }    
+       
+        var lang=localStorage.getItem('lang');
+        localStorage.getItem('lang');   
+        if(lang==("en")){
+        document.documentElement.setAttribute("lang", 'en');
+        $('[lang="en"]').show();
+        $('[lang="tr"]').hide();
+        $('[lang="ar"]').hide();
+        
+    }
+        
+        
+        if(lang==("tr")){
+        document.documentElement.setAttribute("lang", 'tr');
+        $('[lang="tr"]').show();
+        $('[lang="en"]').hide();
+        $('[lang="ar"]').hide();
+       
+    }
+        
+        if(lang==("ar")){
+        document.documentElement.setAttribute("lang", 'ar');
+        $('[lang="ar"]').show();
+        $('[lang="en"]').hide();
+        $('[lang="tr"]').hide();
+        
+    }});
 
 
-    
+        
 
 
-    
-    $(document).ready(function () {
-        $('#switch-lang-en').click(function() {
+        
+        $(document).ready(function () {
+            $('#switch-lang-en').click(function() {
+                document.documentElement.setAttribute("lang", 'en');
+                $('[lang="tr"]').hide();
+                $('[lang="en"]').show();
+                $('[lang="ar"]').hide();
+               
+                localStorage.setItem('lang', 'en');
+            });
+        });
+        
+
+        $(document).ready(function () {
+            $('#switch-lang-tr').click(function() {
+                document.documentElement.setAttribute("lang", 'tr');
+                $('[lang="tr"]').show();
+                $('[lang="en"]').hide();
+                $('[lang="ar"]').hide();
+                localStorage.setItem('lang', 'tr');
+            });
+        });
+
+
+        $(document).ready(function () {
+            $('#switch-lang-ar').click(function() {
+                document.documentElement.setAttribute("lang", 'ar');
+                $('[lang="ar"]').show();
+                $('[lang="en"]').hide();
+                $('[lang="tr"]').hide();
+                localStorage.setItem('lang', 'ar');
+            });
+        });
+
+        
+        
+        function setlangEN(){                   
+            document.documentElement.setAttribute("lang", 'en');   
             $('[lang="tr"]').hide();
             $('[lang="en"]').show();
             $('[lang="ar"]').hide();
             localStorage.setItem('lang', 'en');
-        });
-    });
-    
 
-    $(document).ready(function () {
-        $('#switch-lang-tr').click(function() {
+        }
+        function setlangTR(){
+            document.documentElement.setAttribute("lang", 'tr');
             $('[lang="tr"]').show();
             $('[lang="en"]').hide();
             $('[lang="ar"]').hide();
             localStorage.setItem('lang', 'tr');
-        });
-    });
 
+        }
 
-    $(document).ready(function () {
-        $('#switch-lang-ar').click(function() {
+        function setlangAR(){
+            document.documentElement.setAttribute("lang", 'ar');
             $('[lang="ar"]').show();
             $('[lang="en"]').hide();
             $('[lang="tr"]').hide();
             localStorage.setItem('lang', 'ar');
-        });
-    });
+
+        }
